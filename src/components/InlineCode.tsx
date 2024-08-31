4b34dfbc1c6adc9b53f5efcebd484dd28db17a6f
kdeng <kdeng@mines.edu>
2024-08-31 12:23:47 -0600

    update post

--- src/components/InlineCode.tsx
+++ src/components/InlineCode.tsx
@@ -7,12 +7,7 @@ interface CodeProps {
 const InlineCode: React.FC<CodeProps> = ({ text }) => {
   return (
     <div className="rounded-lg flex items-center top-0 bg-[#11111b] w-[100%]">
-      <p
-        className="m-2 pl-1 font-mono text-base"
-        style={{ whiteSpace: "pre-line" }}
-      >
-        {text}
-      </p>
+      <p className="m-2 pl-1 font-mono text-base whitespace-pre-wrap">{text}</p>
     </div>
   );
 };
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -7,17 +7,13 @@ const TestPage = () => {
   return (
     <div className="pb-5 text-xl font-serif font-normal">
       <p className="pb-3">
-        As the description said, I had very little experience with anything on
-        the Front End side of web development -- the most experience I had was
-        tinkering with HTML elements in Firefox's inspect element tool, so this
-        week long project proved to have a fair amount of difficulty.
+        I made this blog to have a place to document my coding progress and to
+        widen my skills before my school's upcoming career fair.
       </p>
-      <p className="pb-3">
-        To preface, I wanted to create this website to document my process with
-        creating a compiler for the C language using Rust.
-      </p>
-      <p className="pb-3">
-        This post will be a run-through of my process through this project
+      <p className="pb-6">
+        This post will be a run-through of my process through setting up this
+        blog, both so I can remember how to work with these tools and
+        frameworks, and so maybe it can help someone else in creating a blog.
       </p>
       <p className="pb-3">To start a project with Vite,</p>
       <InlineCode
@@ -30,11 +26,11 @@ const TestPage = () => {
       />
       <p className="py-3">
         Running this command brings up a prompt where I chose the React library
-        for my framework and the TypeScript variant
+        for my framework and the TypeScript as my language.
       </p>
       <p className="pb-3">
         After this, you have to install tailwind, postcss, autoprefixer, and
-        also create the configuration files for them
+        also create config files.
       </p>
       <InlineCode
         text=<>
@@ -55,12 +51,115 @@ const TestPage = () => {
       <p>Replace the existing content section with</p>
       <InlineCode
         text={
-          <>
-            <span></span>
-            content: ["./index.html", "./src/**/*.{"{(js, ts, jsx, tsx)}"}"],
-          </>
+          <>content: ["./index.html", "./src/**/*.{"{(js, ts, jsx, tsx)}"}"],</>
         }
       />
+      <p className="py-3">
+        This should be all for setting up the project; now decide on what type
+        of website you want to make.
+      </p>
+      <p className="pb-3">
+        For my blog, I wanted to have separate pages, one to display posts, and
+        then these posts will link to their respective page. To do this, I used
+        the react-router-dom V6 package to help me route pages to eachother - so
+        I downloaded it with
+      </p>
+      <InlineCode
+        text=<>
+          <span className="text-[#a6e3a1]">npm</span> i -D react-router-dom
+        </>
+      />
+      <p className="py-3">
+        To set up the routing, go into the main.tsx file and add a function like
+        this
+      </p>
+      <InlineCode
+        text=<>
+          <p>
+            <span className="text-[#cba6f7]">const</span>{" "}
+            <span className="text-[#b4befe]">router</span> ={" "}
+            <span className="text-[#89b4fa]">createBrowserRouter(</span>
+            <span className="text-[#b4befe]">[</span>
+          </p>
+          <p className="text-[#cba6f7]">{"  {"}</p>
+          <p className="text-[#a6e3a1]">{"   "} path:"/",</p>
+          <p className="text-[#a6e3a1]">
+            {"   "} element:{"<App />,"}
+          </p>
+          <p className="text-[#a6e3a1]">
+            {"   "} errorElement:{"<ErrorPage />,"}
+          </p>
+          <p className="text-[#a6e3a1]">{"   "} children:[...],</p>
+          <p className="text-[#cba6f7]">{"  },"}</p>
+          <span className="text-[#b4befe]">]</span>
+          <span className="text-[#89b4fa]">);</span>
+        </>
+      />
+      <p className="py-3">
+        Make sure you make a errorPage element before adding it in to the above
+        code though, after making this function, call it within the createRoot
+        function in the same file, within the StrictMode element - in other
+        words, at this in
+      </p>
+      <InlineCode
+        text=<>
+          <span className="text-[#f38ba8]">{"<RouterProvider"}</span>{" "}
+          <span className="text-[#cba6f7]">{"router"}</span>
+          <span className="text-[#b4befe]">{"={"}</span>router
+          <span className="text-[#b4befe]">{"}"}</span>{" "}
+          <span className="text-[#f38ba8]">{"/>"}</span>
+        </>
+      />
+      <p className="py-3">
+        This should be all for the setup of the page with react-router-dom, you
+        can start working on whatever you want for a website. Do remember to
+        frequently abstract your code for React functions, it saved me a lot of
+        time in coding this website up
+      </p>
+      <p className="pb-3">
+        Good luck on whatever you want to make, here are some useful websites I
+        used when creating this website
+      </p>
+      <p>
+        <a
+          className="text-[#b4befe]"
+          href="https://nerdcave.com/tailwind-cheat-sheet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TailwindCSS Cheat Sheet
+        </a>
+      </p>
+      <p>
+        <a
+          className="text-[#b4befe]"
+          href="https://react.dev/learn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Docs
+        </a>
+      </p>
+      <p>
+        <a
+          className="text-[#b4befe]"
+          href="https://reactrouter.com/en/main/start/tutorial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Router Docs
+        </a>
+      </p>
+      <p>
+        <a
+          className="text-[#b4befe]"
+          href="https://github.com/catppuccin/catppuccin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Catppuccin Theme Guide (I love this color scheme so much)
+        </a>
+      </p>
     </div>
   );
 };
